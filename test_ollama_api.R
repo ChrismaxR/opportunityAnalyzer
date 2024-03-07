5b4e36ac2a9aaf9704c1893a9ba1ea6f0ccb1aef
Christian van Gaalen <ctmvangaalen@gmail.com>
2024-03-07 15:01:55 +0100

    some cleaning up

--- test_ollama_api.R
+++ test_ollama_api.R
@@ -1,36 +1,35 @@
 library(httr2)
-library(purrr)
 
 # Ollama API documentatie: https://github.com/ollama/ollama/blob/main/docs/api.md
 
 # Testen hoe een call naar Ollama zou kunnen werken
 # eerst de curl optie:
 
-tictoc::tic()
-test_call <- request("http://localhost:11434/api/generate") |>  
-  req_body_raw(
-    "{
-      \"model\": \"llama2\",\n  
-      \"prompt\": \"Which city is the seat of government of Belgium\",\n  
-      \"format\": \"json\",\n  
-      \"stream\": false\n
-    }", 
-    "application/x-www-form-urlencoded") #|>  
-  #req_perform()
-tictoc::toc()
+# tictoc::tic()
+# test_call <- request("http://localhost:11434/api/generate") |>  
+#   req_body_raw(
+#     "{
+#       \"model\": \"llama2\",\n  
+#       \"prompt\": \"Which city is the seat of government of Belgium\",\n  
+#       \"format\": \"json\",\n  
+#       \"stream\": false\n
+#     }", 
+#     "application/x-www-form-urlencoded") #|>  
+#   #req_perform()
+# tictoc::toc()
 
 # dan testen met de json body:
-request("http://localhost:11434/api/generate") |>  
-  req_body_json(
-    data = list(
-      model  = "mistral", 
-      prompt = "Which city is the capital of France?", 
-      format = "json",
-      stream = F
-    ),
-    type = "application/json"
-  ) #|> 
-  #req_perform()
+# request("http://localhost:11434/api/generate") |>  
+#   req_body_json(
+#     data = list(
+#       model  = "mistral", 
+#       prompt = "Which city is the capital of France?", 
+#       format = "json",
+#       stream = F
+#     ),
+#     type = "application/json"
+#   ) #|> 
+#   #req_perform()
 
 #resp_body_json(test_call)
 
@@ -72,8 +71,8 @@ ollamaCaller <- function(model, prompt) {
 }
 
 # Testen van de functie om performance te checken
-test_calls <- map2_df(
+test_calls <- purrr::map2_df(
   .f = ollamaCaller, 
   .x = c("llama2", "mistral"),
-  .y = c("What is the capital of Berlin", "What is the capital of Berlin")
+  .y = c("What is the capital of Germany", "What is the capital of Germany")
 )
